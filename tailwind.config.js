3823d9651c7bedb6e951a472664fdef553ba4a9f
Joalseca <bitxelroads@gmail.com>
2025-10-23 15:09:42 -0500

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,23 @@ module.exports = {
       "./src/**/*.{html,js}"
     ],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+            'valenta-orange': '#FF5400',
+            'valenta-navy': '#0a2735',
+            'valenta-teal': '#00b2b3',
+            'valenta-teal-dark': '#047f80',
+            'valenta-teal-darker': '#044b4b',
+            'valenta-gray': {
+                light: '#f8f9fa',
+                DEFAULT: '#5a6c7d',
+            },
+            'valenta-content': '#2c3e50',
+        },
+        fontFamily: {
+            sans: ['DM Sans', 'sans-serif'],
+        }
+      },
     },
     plugins: [],
   }
-  
